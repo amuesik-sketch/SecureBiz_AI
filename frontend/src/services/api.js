6bdc9fd7ebc0d9b35b6e0bd81f67adf567affe22
Kevin Amuesi <amuesik@gmail.com>
2026-08-01 19:05:18 +0000

    Connect frontend to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://securebiz-ai.onrender.com/api",
 
   headers: {
+    Accept: "application/json",
     "Content-Type": "application/json",
   },
 });
